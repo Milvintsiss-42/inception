ab1e8de2f3dd1590d48fdd49a338e1e2e16e1610
Milvintsiss <milvintsiss@gmail.com>
2024-01-02 02:55:27 +0100

    Moves tls certificate and key in a dedicated folder

--- srcs/requirements/nginx/tools/create_tls_certificate.sh
+++ srcs/requirements/nginx/tools/create_tls_certificate.sh
@@ -7,14 +7,15 @@ O="42"
 CN="ple-stra"
 EMAIL_ADRESS="ple-stra@student.42.fr"
 
+mkdir -p /etc/nginx/tls
 # Create a new self-signed certificate & key and set them to read-only.
 openssl req -new \
 		-newkey rsa:4096 -x509 -sha256 \
 		-days 365 \
 		-nodes \
 		-subj "/C=$C/ST=$ST/L=$L/O=$O/CN=$CN/emailAddress=$EMAIL_ADRESS" \
-		-out /etc/nginx/tls_certificate.pem \
-		-keyout /etc/nginx/tls_certificate_key.pem \
+		-out /etc/nginx/tls/tls_certificate.pem \
+		-keyout /etc/nginx/tls/tls_certificate_key.pem \
 	&& chmod 400 \
-		/etc/nginx/tls_certificate.pem \
-		/etc/nginx/tls_certificate_key.pem
+		/etc/nginx/tls/tls_certificate.pem \
+		/etc/nginx/tls/tls_certificate_key.pem
