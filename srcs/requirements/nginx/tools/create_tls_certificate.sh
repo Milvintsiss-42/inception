197f80d27a29cfbf4684aabec704d6fec4fd29dc
Milvintsiss <milvintsiss@gmail.com>
2024-01-02 02:21:32 +0100

    Moves tls certifate creation process in a dedicated script file

---
+++ srcs/requirements/nginx/tools/create_tls_certificate.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+C="FR"
+ST="Ile-de-France"
+L="Paris"
+O="42"
+CN="ple-stra"
+EMAIL_ADRESS="ple-stra@student.42.fr"
+
+# Create a new self-signed certificate & key and set them to read-only.
+openssl req -new \
+		-newkey rsa:4096 -x509 -sha256 \
+		-days 365 \
+		-nodes \
+		-subj "/C=$C/ST=$ST/L=$L/O=$O/CN=$CN/emailAddress=$EMAIL_ADRESS" \
+		-out /etc/nginx/tls_certificate.pem \
+		-keyout /etc/nginx/tls_certificate_key.pem \
+	&& chmod 400 \
+		/etc/nginx/tls_certificate.pem \
+		/etc/nginx/tls_certificate_key.pem
