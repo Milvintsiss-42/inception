181e13edf4d56c10763c0427d9850016b15e641e
Milvintsiss <milvintsiss@gmail.com>
2024-01-04 23:05:40 +0100

    Setups Wordpress defaults when installing

--- srcs/requirements/wordpress/tools/install_and_launch_wordpress.sh
+++ srcs/requirements/wordpress/tools/install_and_launch_wordpress.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-if [ ! -f "/var/www/html/wp-config.php" ]; then
+cd /var/www/html
+if [ ! -f "wp-config.php" ]; then
 	echo "Installing WordPress on container..."
-	mv wordpress/* /var/www/html/
+
+	mv /wordpress/* .
+
+	wp config create --dbname='wordpress' --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost='mariadb:3306' --allow-root
+	wp core install --url="$DOMAIN_NAME" --title="Macarena" --admin_user="$WP_ADMIN_USERNAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root
+	wp user create "$WP_BASIC_USER_USERNAME" "$WP_BASIC_USER_EMAIL" --user_pass="$WP_BASIC_USER_PASSWORD" --allow-root
+
 	echo "Done."
 fi
-rm -rf wordpress
+rm -rf /wordpress
 
-cd /var/www/html
 /usr/sbin/php-fpm7.4 -F
